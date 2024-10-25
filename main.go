4dc64c6bf172f28f2b586249e749ad156a9f4e09
Paulo Roberto Chiardia Neto <97291302+paulochiaradia@users.noreply.github.com>
2024-10-25 10:50:33 -0300

    Add handlers

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
+// homeHandler writes a byte slice containing "Hello from Snppetbox"
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	//Check if the current request URL path exactly matches "/"
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// snippetView Display a specifc snippet
+func snippetViewHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specifc snippet"))
+}
+
+// snippetCreate Create a new snippet
+func snippetCreateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
+}
 func main() {
-	fmt.Println("Hello")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/snippet/view", snippetViewHandler)
+	mux.HandleFunc("/snippet/create", snippetCreateHandler)
+	log.Println("Stating server on:4000")
+	log.Fatal(http.ListenAndServe(":4000", mux))
 }
